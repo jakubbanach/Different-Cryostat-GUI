0bf26ce9f31c8f17e00d32e3bbab57443381e61b
jakubbanach <jakbanach@student.agh.edu.pl>
2022-11-21 14:00:51 +0100

    Create main.py
    First version of the project

---
+++ main.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+from PyQt6.QtCore import QDateTime, Qt, QTimer
+from PyQt6.QtWidgets import (QApplication, QCheckBox, QComboBox, QDateTimeEdit,
+        QDial, QDialog, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit,
+        QProgressBar, QPushButton, QRadioButton, QScrollBar, QSizePolicy,
+        QSlider, QSpinBox, QStyleFactory, QTableWidget, QTabWidget, QTextEdit,
+        QVBoxLayout, QWidget)
+
+class App(QDialog):
+    def __init__(self, parent=None):
+        super(App, self).__init__(parent)
+
+        self.originalPalette = QApplication.palette()
+
+        styleComboBox = QComboBox()
+        styleComboBox.addItems(QStyleFactory.keys())
+
+        styleLabel = QLabel('&Style:')
+        styleLabel.setBuddy(styleComboBox)
+
+        self.useStylePaletteCheckbox = QCheckBox("&Use style's standard palette")
+        self.useStylePaletteCheckbox.setChecked(True)
+
+        self.setWindowTitle('DATAPLOT')
+        self.changeStyle('windowsvista')
+
+
+
+    # self.changeStyle("Windows")
+        self.createTopLeftGroupBox()
+    # self.createTopRightGroupBox()
+    # self.createBottomLeftTabWidget()
+    # self.createBottomRightGroupBox()
+
+        topLayout = QVBoxLayout()
+        topLayout.addWidget(styleLabel)
+        topLayout.addWidget(styleComboBox)
+        topLayout.addStretch(1)
+        topLayout.addWidget(self.useStylePaletteCheckbox)
+
+
+        mainLayout = QGridLayout()
+        mainLayout.addLayout(topLayout, 0, 0, 1, 2)
+        mainLayout.addWidget(self.topLeftGroupBox, 1, 0)
+        mainLayout.setRowStretch(1, 1)
+        # mainLayout.setRowStretch(2, 1)
+        # mainLayout.setColumnStretch(0, 1)
+        mainLayout.setColumnStretch(1, 1)
+        self.setLayout(mainLayout)
+
+
+    def changeStyle(self, styleName):
+        QApplication.setStyle(QStyleFactory.create(styleName))
+        self.changePalette()
+
+
+    def changePalette(self):
+        if (self.useStylePaletteCheckbox.isChecked()):
+            QApplication.setPalette(QApplication.style().standardPalette())
+        else:
+            QApplication.setPalette(self.originalPalette)
+
+    def createTopLeftGroupBox(self):
+        self.topLeftGroupBox = QGroupBox("Group 1")
+
+        radioButton1 = QRadioButton("Radio button 1")
+        radioButton2 = QRadioButton("Radio button 2")
+        radioButton3 = QRadioButton("Radio button 3")
+        radioButton1.setChecked(True)
+
+        checkBox = QCheckBox("Tri-state check box")
+        checkBox.setTristate(True)
+        checkBox.setCheckState(Qt.CheckState.PartiallyChecked)
+
+        layout = QVBoxLayout()
+        layout.addWidget(radioButton1)
+        layout.addWidget(radioButton2)
+        layout.addWidget(radioButton3)
+        layout.addWidget(checkBox)
+        layout.addStretch(1)
+        self.topLeftGroupBox.setLayout(layout)
+
+if __name__ == '__main__':
+
+    import sys
+
+    app = QApplication(sys.argv)
+    window = App()
+    window.resize(1600, 1200)
+    window.show()
+    sys.exit(app.exec())
